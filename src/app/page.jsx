93fc9e31e26cec697e1e5ed0e430b455b7238400
glitchnobody <siddhantvij001@gmail.com>
2025-01-11 20:17:53 +0530

    first

---
+++ src/app/page.jsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <p className=" text-2xl font-title font-extrabold ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+        officia.
+      </p>
+      <p className=" text-2xl font-body font-extrabold ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+        officia.
+      </p>
+      <p className=" text-2xl font-cta font-extrabold ">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis,
+        officia.
+      </p>
+    </div>
+  );
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,8 +9,19 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          100: "#FFF6E9",
+          150: "#FFF1D4",
+          200: "#EE7F09",
+          250: "#FFBB00",
+        },
+        secondary: "#252525",
+        crimson: "#EE092D",
+      },
+      fontFamily: {
+        title: ["Comfortaa", "sans-serif"],
+        body: ["Satoshi", "sans-serif"],
+        cta: ["Raleway", "sans-serif"],
       },
     },
   },
